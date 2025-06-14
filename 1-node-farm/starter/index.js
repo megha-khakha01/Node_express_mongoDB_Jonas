f681565ab19b547adeb39f870f758a457f26cade
Megha Khakha <meghakhakha148@gmail.com>
2025-06-14 11:50:06 +0530

    ongoing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = required('fs');
 
-// Blocking , synchronous way
+//************************ Blocking , synchronous way******************//
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
  console.log(textIn);
 const textOut = `This is what we know about the avocado: ${textIn}.\ncreated on ${Date.now()}`;
@@ -8,13 +8,18 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written successfully!');
  
 
-//Non - blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile('./txt/${data1}.txt', 'utf-8', (err,data2) => {
-        console.log(data2);
-        fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-            console.log(data);
+//**********************Non - blocking, asynchronous way****************//
+fs.readFile('./text/start.txt' ,'utf-8', (err,data1) => {
+    fs.readFile('./text/start.txt' ,'utf-8', (err,data2) => {
+         console.log(data);
+         fs.readFile('./text/start.txt' ,'utf-8', (err,data3) =>{
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', '${data2}\n${data3}','utf-8', err => {
+                console.log('Your file has been written');
+            })
         });
-    });
+     });
 });
-console.log('Will read file ! );
+console.log('Will read file');
+
