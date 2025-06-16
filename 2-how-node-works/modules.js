e892a2fa776c5070f9d6a93475ff9d378bbeab32
Megha Khakha <meghakhakha148@gmail.com>
2025-06-16 16:06:30 +0530

    progress

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -1,2 +1,9 @@
-console.log(arguments);
-console.log(require("module").wrapper);
+//console.log(arguments);
+//console.log(require("module").wrapper);
+
+// module.exports
+const C = require('./test-module-1');
+const calc1 = new C();
+console.log(calc1.add(2, 5));
+
+// exports
--- 2-how-node-works/test-module-1.js
+++ 2-how-node-works/test-module-1.js
@@ -1,5 +1,20 @@
 class Calculator{
-    add(a, b) {
+//     add(a, b) {
+//         return a + b;
+//         }
+
+//         multiply(a, b){
+//             return a*b;
+//         }
+
+//         divide(a, b){
+//             return a/b;
+//         }
+// }
+
+
+module.exports = class {
+add(a, b) {
         return a + b;
         }
 
@@ -10,4 +25,4 @@ class Calculator{
         divide(a, b){
             return a/b;
         }
-}
+};
