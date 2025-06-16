2f545eeead504ad22c05195ed500557b4fb29099
Megha Khakha <meghakhakha148@gmail.com>
2025-06-16 16:56:18 +0530

    progress

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -8,6 +8,11 @@ console.log(calc1.add(2, 5));
 
 // exports
 //const calc2 = require("./test-module-2");
-const {add, multiply, divide} = require("./test-module-2");
+const {add, multiply} = require("./test-module-2");
 
 console.log(multiply(2,5));
+
+//caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
