a06b178af5fef7eb8a538e6dab29223ceeb5d5b2
Megha Khakha <meghakhakha148@gmail.com>
2025-06-16 14:15:38 +0530

    progress

--- 2-how-node-works/events.js
+++ 2-how-node-works/events.js
@@ -1,7 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
+class Sales extends EventEmitter{
+    constructor(){
+        super();
+    }
+}
 
-const myEmitter = new EventEmitter();
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
     console.log("There was a new sale!");
@@ -11,4 +17,30 @@ myEmitter.on("newSale", () => {
     console.log("Constumer name: Jonas");
 });
 
-myEmitter.emit("newSale");
+myEmitter.on('newSale', stock => {
+console.log('There are now ${stock} items left in stocks.');
+});
+
+myEmitter.emit("newSale", 9);
+
+///////////////////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    console.log("Request reeived!");
+    console.log(req.url);
+    res.end("Request received");
+});
+
+server.on("request",(req, res) => {
+    console.log("Another request ,-😊");
+});
+
+server.on("close",() => {
+    console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1",() => {
+    console.log("Waiting for requests....");
+});
