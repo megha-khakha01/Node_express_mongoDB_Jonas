0f3961ca2c709f64fc92ae06fa3dd4b0333e94b1
Megha Khakha <meghakhakha148@gmail.com>
2025-06-16 13:39:08 +0530

    progress

--- 2-how-node-works/event-lopp.js
+++ 2-how-node-works/event-lopp.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 const start = Date.now();
+process.env.UV_THEADPOOL_SIZE = 4;
 
 setTimeout(() => console.log ("Timer 1 finished"), 0);
 setImmediate(() => console.log("Imediate 1 finished"));
@@ -12,14 +13,27 @@ fs.readFile("test-file.txt" , () => {
 
 
     setTimeout(() => console.log("Timer 2 finished"), 0);
-        setTimeout(() => console.log("Timer 3 finished"), 3000);
-        setImmediate(() => console.log("Timer 2 finished"));
+    setTimeout(() => console.log("Timer 3 finished"), 3000);
+    setImmediate(() => console.log("Timer 2 finished"));
 
-        process.nextTrick(()=> console.log("Process.nextTrick"))
+    process.nextTrick(()=> console.log("Process.nextTrick"))
 
-        crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-            console.log(Date.now()-start,"Password encrypted")
+        crypto.pbkdf25sync('password', 'salt', 10000, 1024, "sha512");
+        console.log(Date.now() - start, "Password encrypted");
+
+         crypto.pbkdf25sync('password', 'salt', 10000, 1024, "sha512");
+        console.log(Date.now() - start, "Password encrypted");
+
+
+         crypto.pbkdf25sync('password', 'salt', 10000, 1024, "sha512");
+        console.log(Date.now() - start, "Password encrypted");
+
+
+         crypto.pbkdf25sync('password', 'salt', 10000, 1024, "sha512");
+        console.log(Date.now() - start, "Password encrypted");
+
+
+        
         });
-});
 
 console.log ("Hello from the top-level code");
