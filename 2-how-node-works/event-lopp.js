746ed338962a4cbb4cf8aaae4e16363b94935902
Megha Khakha <meghakhakha148@gmail.com>
2025-06-16 13:31:18 +0530

    progress

---
+++ 2-how-node-works/event-lopp.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+
+setTimeout(() => console.log ("Timer 1 finished"), 0);
+setImmediate(() => console.log("Imediate 1 finished"));
+
+fs.readFile("test-file.txt" , () => {
+    console.log("I/10 finished");
+        console.log("----------------------");
+
+
+    setTimeout(() => console.log("Timer 2 finished"), 0);
+        setTimeout(() => console.log("Timer 3 finished"), 3000);
+        setImmediate(() => console.log("Timer 2 finished"));
+
+        process.nextTrick(()=> console.log("Process.nextTrick"))
+
+        crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+            console.log(Date.now()-start,"Password encrypted")
+        });
+});
+
+console.log ("Hello from the top-level code");
